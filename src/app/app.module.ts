7cb839c188f6ef7e020932dff3bd23482db22897
borgella <jmborgella@hotmail.com>
2017-06-01 12:21:43 -0400

    finish organisation design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DetailComponent } from './detail/detail.component';
+import { FooterComponent } from './footer/footer.component';
+import { OrganisationComponent } from './organisation/organisation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    DetailComponent,
+    FooterComponent,
+    OrganisationComponent
   ],
   imports: [
     BrowserModule,
