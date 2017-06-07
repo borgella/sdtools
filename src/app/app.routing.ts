024bfd8abb1ea8a3a260ad3966d8cdce53ec1be5
borgella <jmborgella@hotmail.com>
2017-06-06 21:23:32 -0400

    refactoring of routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GeneralComponent } from 'app/general/general.component';
-import { EndpointsComponent } from 'app/endpoints/endpoints.component';
-import { SpecificationComponent } from 'app/specification/specification.component';
-import { DetailComponent } from 'app/detail/detail.component';
+import { GeneralComponent } from 'app/dashboard/general/general.component';
+import { EndpointsComponent } from 'app/dashboard/endpoints/endpoints.component';
+import { SpecificationComponent } from 'app/dashboard/specification/specification.component';
+import { DetailComponent } from 'app/dashboard/detail/detail.component';
 import { ProductionComponent } from 'app/production/production.component';
+import { DashboardComponent } from 'app/dashboard/dashboard.component';
 
 
 const routes: Routes = [
-    { path: '', component: GeneralComponent},
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+    { path: 'dashboard', component: DashboardComponent, children: [
+        { path: '', component: GeneralComponent},
+        { path: 'endpoints', component: EndpointsComponent},
+        { path: 'specification', component: SpecificationComponent},
+        { path: 'detail/:id', component: DetailComponent}
+    ]},
     { path: 'production', component: ProductionComponent},
-    { path: 'endpoints', component: EndpointsComponent},
-    { path: 'specification', component: SpecificationComponent},
-    { path: 'detail/:id', component: DetailComponent},
     { path: 'production/:id', component: ProductionComponent}
 ];
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ISummary } from 'app/model/summary/isummary';
+import { DocumentService } from 'app/document.service';
+import { CommunicationService } from 'app/communication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  private allSummaries: Array<ISummary>;
+  private summary: ISummary;
 
-  ngOnInit() {
+  constructor(private _documentService: DocumentService, private _communication: CommunicationService, private router: Router) { }
+
+  public ngOnInit(): void {
+    this.allSummaries = this._documentService.getSummaries();
+  }
+
+  public viewSingleSummary(id: string): void {
+    this.summary = this._documentService.singleSummary(id);
+    this._communication.subject.next(this.summary);
+    this.router.navigate(['production']);
+
   }
 
 }
